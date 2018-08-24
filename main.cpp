74057564c6258e6e7b0144c038f8652ff1fb3128
Rayner <raynersulyak@gmail.com>
2018-08-24 01:07:02 -0300

    added a condition in case the opening of file fails

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 int main(){
 	std::string s = "#include <stdio.h>\nint main(){ ";
 	FILE *f;
-	int a, b;
+	int a, b, result;
+
 	printf("Enter two integers: ");
 	scanf("%d %d", &a, &b);
 	s += "int x = " + std::to_string(a) + "; ";
 	s += "for(int i = 0; i < " + std::to_string(b) + "; i++) x++; ";
 	s += "return x; }";
+
 	f = fopen("sumfile.c", "w");
+	if(f == NULL){ //if the file doesnt open properly
+		printf("Error!\n");
+		exit(-1);
+	}
+
 	fprintf(f, "%s", s.c_str());
 	fclose(f);
+
 	system("gcc -std=c99 -o sumf sumfile.c");
-	int result = system("sumf");
+	result = system("sumf");
+	
 	printf("Sum is %d\n", result);
 	return 0;
 }
